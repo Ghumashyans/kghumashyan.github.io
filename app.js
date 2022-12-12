7c3031e3daf638e437e1b735f483bc0d26a5ea95
Ghumashyans <120367178+Ghumashyans@users.noreply.github.com>
2022-12-12 12:10:59 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,92 @@
+let points = 0
+
+let koryun0 = prompt("Վորն է Հայաստանի մայրաքաղաքը");
+   if(koryun0 === "Երևան" || koryun0 === "երևան"){
+     points = points + 1;
+}
+
+
+let koryun1 = prompt("Վորն է Ռուսաստանի մայրաքաղաքը");
+   if(koryun1 === "Մոսկվա" || koryun1 === "մոսկվա"){
+     points = points + 1;
+}
+
+
+
+let koryun2 = prompt("Վորն է ԱՄՆի մայրաքաղաքը");
+   if(koryun2 === "Վաշինգտոն" || koryun2 === "վաշինգտոն"){
+     points = points + 1;
+}
+
+
+
+let koryun3 = prompt("Վորն է Արցախի մայրաքաղաքը");
+   if(koryun3 === "Ստեփանակերտ" || koryun3 === "ստեփանակերտ"){
+     points = points + 1;
+}
+
+
+
+
+let koryun4 = prompt("Վորն է Ճապոնիաի մայրաքաղաքը");
+   if(koryun0 === "Տոկիո" || koryun4 === "տոկիո"){
+     points = points + 1;
+}
+
+
+
+
+let koryun5 = prompt("Վորն է Իրանի մայրաքաղաքը");
+   if(koryun0 === "Թեհրան" || koryun5 === "թեհրան"){
+     points = points + 1;
+}
+
+
+
+let koryun6 = prompt("Վորն է  Ադրբեջանի մայրաքաղաքը");
+   if(koryun6 === "Բաքու" || koryun6 === "բաքու"){
+     points = points + 1;
+}
+
+
+
+let koryun7 = prompt("Վորն է  Բելգիաի մայրաքաղաքը");
+   if(koryun7 === "Բրյուսել" || koryun7 === "բրյուսել"){
+     points = points + 1;
+}
+
+let koryun8 = prompt("Վորն է Թուրքիա մայրաքաղաքը");
+   if(koryun8 === "Անկարա" || koryun8 === "անկարա"){
+     points = points + 1;
+}
+
+
+let koryun9 = prompt("Վորն է Լիբանանի մայրաքաղաքը");
+   if(koryun9 === "Բերյութ" || koryun9 === "բերյութ"){
+     points = points + 1;
+}
+
+
+
+
+    alert("dzer miavornery" +points+ " " )
+
+
+
+
+ 
+   
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+ 
